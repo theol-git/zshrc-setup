83046343e20aa19cc6f529644a4f8879a3116a0f
Theo <theo.lettermann@gmail.com>
2023-04-28 17:50:11 +0000

    install python3-venv when installing pyenv

--- setup.sh
+++ setup.sh
@@ -38,7 +38,7 @@ case "$choice" in
     # Install pyenv dependencies
     sudo apt install build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python3-venv -y
 
     # Install pyenv
     curl https://pyenv.run | bash
